37d0e73befec022e1f9925a527934a90ad31a086
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-07-13 17:24:03 +0530

    Time: 3 ms (55.19%) | Memory: 12.4 MB (81.54%) - LeetSync

--- 43-multiply-strings/multiply-strings.py
+++ 43-multiply-strings/multiply-strings.py
@@ -1,24 +1,3 @@
-class Solution:
+class Solution(object):
     def multiply(self, num1, num2):
-        if num1 == "0" or num2 == "0":
-            return "0"
-
-        n, m = len(num1), len(num2)
-        result = [0] * (n + m)
-
-        for i in reversed(range(n)):
-            for j in reversed(range(m)):
-                mul = (ord(num1[i]) - 48) * (ord(num2[j]) - 48)
-                p1 = i + j
-                p2 = i + j + 1
-
-                sum_ = mul + result[p2]
-                result[p2] = sum_ % 10
-                result[p1] += sum_ // 10
-
-        res = ""
-        for num in result:
-            if not (len(res) == 0 and num == 0):
-                res += chr(num + 48)
-
-        return res
+        return str(int(num1) * int(num2))
