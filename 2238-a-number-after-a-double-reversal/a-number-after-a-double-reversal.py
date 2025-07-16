ba52c55ec0f97eb7f7c9838fe375c0adc12a0198
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-07-16 19:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (50.18%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -1,13 +1,3 @@
-class Solution:
-    def reverseInteger(self, n):
-        sign = -1 if n < 0 else 1
-        rev = int(str(abs(n))[::-1])
-        result = sign * rev
-        if result < -2**31 or result > 2**31 - 1:
-            return 0
-        return result
-
+class Solution(object):
     def isSameAfterReversals(self, num):
-        r = self.reverseInteger(num)
-        rr = self.reverseInteger(r)
-        return rr == num
+        return num == int(str(int(str(num)[::-1]))[::-1])
