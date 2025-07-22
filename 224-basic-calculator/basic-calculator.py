d96f608d9ce71828f0dd1735606ed5be2642c077
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-07-22 15:24:39 +0530

    Time: 54 ms (77.72%) | Memory: 13.1 MB (93.22%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s):
+        stack = []
+        result = 0
+        num = 0
+        sign = 1
+
+        for char in s:
+            if char.isdigit():
+                num = num * 10 + int(char)
+            elif char in '+-':
+                result += sign * num
+                sign = 1 if char == '+' else -1
+                num = 0
+            elif char == '(':
+                stack.append(result)
+                stack.append(sign)
+                result = 0
+                sign = 1
+            elif char == ')':
+                result += sign * num
+                result *= stack.pop()
+                result += stack.pop()
+                num = 0
+
+        return result + sign * num
