fb0fecf8893776319e04951fba786d7a0188f497
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-08-23 18:41:13 +0530

    Time: 204 ms (13.21%) | Memory: 91.2 MB (96.33%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDepth(self, root):
+        if not root:
+            return 0   # Agar tree hi empty hai, depth = 0
+        
+        stack = [(root, 1)]   # Stack me (node, depth) store karenge
+        minDepth = float("inf")   # Initially infinite rakha, taaki min compare ho sake
+        
+        while stack:
+            node, depth = stack.pop()   # Top element nikala
+            
+            # Agar leaf node mila (dono child None)
+            if not node.left and not node.right:
+                minDepth = min(minDepth, depth)
+            
+            # Left child ko stack me daal
+            if node.left:
+                stack.append((node.left, depth + 1))
+            
+            # Right child ko stack me daal
+            if node.right:
+                stack.append((node.right, depth + 1))
+        
+        return minDepth
