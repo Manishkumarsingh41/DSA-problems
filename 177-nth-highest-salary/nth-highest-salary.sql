47a836de34b6a524980e5043927745dbb2ba1aec
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-09-06 17:35:38 +0530

    Time: 369 ms (99.81%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,13 +1,17 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+#Ek local variable 'M' bana rahe hain, bas ek number store karne ke liye.
+  DECLARE M INT;
+  
+ # N-1 ki value 'M' me daal rahe hain, taki OFFSET ko direct number mile.
+  SET M = N - 1;
+  
   RETURN (
-      SELECT DISTINCT Salary
-      FROM (
-          SELECT
-              Salary,
-              DENSE_RANK() OVER (ORDER BY Salary DESC) AS rnk
-          FROM Employee
-      ) AS ranked_salaries
-      WHERE rnk = N
+    SELECT DISTINCT Salary
+    FROM Employee
+   #Salaries ko bade se chhote order me lagana hai
+    ORDER BY Salary DESC
+   #Ab M salaries ko chhod kar, bas ek salary lena hai.
+    LIMIT 1 OFFSET M
   );
 END
