5f34b58b36efdbecc492e06a1a5da3cccd4ebdf2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-09-07 17:32:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (60.00%) - LeetSync

---
+++ 458-poor-pigs/poor-pigs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def poorPigs(self, buckets, minutesToDie, minutesToTest):
+        tests = minutesToTest // minutesToDie
+        pigs = 0
+        while (tests + 1) ** pigs < buckets:
+            pigs += 1
+        return pigs
