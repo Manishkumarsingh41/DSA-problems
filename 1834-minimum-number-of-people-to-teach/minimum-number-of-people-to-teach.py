e649008b60fc1ea8011787e757080b877f0032e1
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-09-10 17:06:19 +0530

    Time: 68 ms (52.94%) | Memory: 23.3 MB (58.82%) - LeetSync

---
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+class Solution(object):
+    def minimumTeachings(self, n, languages, friendships):
+        # Har user ki languages ko list se set bana rahe hain, taaki common language check karna fast ho.
+        lang_sets = [set(lang_list) for lang_list in languages]
+
+        # Yahan un users ko store karenge jo apne dosto se baat nahi kar sakte.
+        users_in_need = set()
+        for u, v in friendships:
+            # friendships 1-indexed hai, use 0-indexed bana rahe hain.
+            user1, user2 = u - 1, v - 1
+            # Agar dono ke languages mein kuch bhi common nahi hai...
+            if not lang_sets[user1].intersection(lang_sets[user2]):
+                # ...toh dono users ko set mein add kar do.
+                users_in_need.add(user1)
+                users_in_need.add(user2)
+
+        # Agar sab dost aapas mein baat kar sakte hain, toh kuch nahi karna.
+        if not users_in_need:
+            return 0
+        
+        # 'users_in_need' wale group mein har language kitni baar aati hai, woh count kar rahe hain.
+        lang_counts = Counter()
+        for user_id in users_in_need:
+            for lang in lang_sets[user_id]:
+                lang_counts[lang] += 1
+
+        # Check kar rahe hain ki sabse common language kaun si hai aur kitne log bolte hain.
+        max_frequency = 0
+        if lang_counts:
+            max_frequency = max(lang_counts.values())
+
+        # Total users jinko help chahiye MINUS woh log jo pehle se hi sabse common language jaante hain.
+        return len(users_in_need) - max_frequency
