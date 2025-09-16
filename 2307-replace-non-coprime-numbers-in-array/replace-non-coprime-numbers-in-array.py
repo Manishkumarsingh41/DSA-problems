a6b127084a07586c17802035ed9cf6359ade0e50
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-09-16 22:34:49 +0530

    Time: 416 ms (50.00%) | Memory: 27.1 MB (50.00%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def replaceNonCoprimes(self, nums):
+        # Custom gcd function (Euclidean Algorithm)
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        
+        stack = []
+        
+        for num in nums:
+            stack.append(num)
+            # Jab tak stack ke last 2 elements non-coprime hain → merge
+            while len(stack) > 1:
+                g = gcd(stack[-1], stack[-2])
+                if g == 1:   # agar coprime hai toh break
+                    break
+                # non-coprime → LCM nikalo
+                lcm = stack[-1] * stack[-2] // g
+                stack.pop()
+                stack.pop()
+                stack.append(lcm)
+        
+        return stack
