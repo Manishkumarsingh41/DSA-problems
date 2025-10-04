6163422d4c327dadb77ea3d1f2faa76f679c9661
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-10-04 14:32:29 +0530

    Time: 81 ms (90.05%) | Memory: 20.7 MB (55.10%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,21 +1,15 @@
 class Solution:
     def maxArea(self, height):
-        left, right = 0, len(height) - 1
-        max_area = 0
-        
-        while left < right:
-            # Current area
-            h = min(height[left], height[right])
-            w = right - left
-            area = h * w
-            
-            # Update max area
-            max_area = max(max_area, area)
-            
-            # Move pointer with smaller height
-            if height[left] < height[right]:
-                left += 1
+        l = 0
+        r = len(height) - 1
+        m = 0
+        while l < r:
+            if height[l] >= height[r]:
+                tmp = height[r] * (r - l)
+                m = max(m, tmp)
+                r -= 1
             else:
-                right -= 1
-                
-        return max_area
+                tmp = height[l] * (r - l)
+                m = max(m, tmp)
+                l += 1
+        return m
