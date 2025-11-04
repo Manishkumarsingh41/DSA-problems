7a33eaeda0b408b61f2dbd2eb3636e5fc1cfc3a0
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-11-04 21:02:03 +0530

    Time: 28 ms (93.94%) | Memory: 12.4 MB (81.82%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.py
@@ -1,17 +1,10 @@
-from typing import List
-from collections import Counter
-class Solution:
-    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        res = []
-        freq = Counter(nums[:k])
-        for i in range(len(nums) - k + 1):
-            top = sorted(freq.items(), key=lambda t: (-t[1], -t[0]))[:x]
-            res.append(sum(n * freq[n] for n, _ in top))
-            if i + k < len(nums):
-                out_num, in_num = nums[i], nums[i + k]
-                freq[out_num] -= 1
-                if freq[out_num] == 0:
-                    del freq[out_num]
-                freq[in_num] += 1
-        
-        return res
+class Solution(object):
+    def findXSum(self, nums, k, x):
+        n=len(nums);ans=[]
+        for i in range(n-k+1):
+            c={}
+            for v in nums[i:i+k]:c[v]=c.get(v,0)+1
+            s=sorted(c.items(),key=lambda a:(-a[1],-a[0]))
+            top={a for a,_ in s[:x]}
+            ans.append(sum(v for v in nums[i:i+k] if v in top))
+        return ans
