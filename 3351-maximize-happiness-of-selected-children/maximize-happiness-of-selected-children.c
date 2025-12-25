be5435e69436f6dbe28e529e5ec771a0257b8946
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2025-12-25 16:59:04 +0530

    Time: 210 ms (11.11%) | Memory: 24.9 MB (88.89%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.c
@@ -0,0 +1,19 @@
+int compare(const void* a, const void* b) {
+    return (*(int*)b - *(int*)a);
+}
+
+long long maximumHappinessSum(int* happiness, int happinessSize, int k) {
+    qsort(happiness, happinessSize, sizeof(int), compare);
+    
+    long long total = 0;
+    
+    for (int i = 0; i < k; i++) {
+        int contribution = happiness[i] - i;
+        if (contribution <= 0) {
+            break;
+        }
+        total += contribution;
+    }
+    
+    return total;
+}
