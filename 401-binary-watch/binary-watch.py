034cf24e7ee24a32b849fc1bdc3d23aa0ff56618
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2026-02-17 16:44:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (13.49%) - LeetSync

--- 401-binary-watch/binary-watch.py
+++ 401-binary-watch/binary-watch.py
@@ -1,24 +1,11 @@
+from typing import List
+
 class Solution:
-    def readBinaryWatch(self, num):
-        res = []
-
-        def backtrack(i, cnt, h, m):
-            if h > 11 or m > 59:  # invalid time
-                return
-            if cnt == num:  # exact LEDs on -> valid time
-                res.append("{}:{:02d}".format(h, m))  # add formatted time
-                return
-            if i == 10:  # last LED tak pahunch gaye
-                return
-
-            # LED ON
-            if i < 4:  # hour LED
-                backtrack(i+1, cnt+1, h | (1<<i), m)
-            else:  # minute LED
-                backtrack(i+1, cnt+1, h, m | (1<<(i-4)))
-
-            # LED OFF
-            backtrack(i+1, cnt, h, m)  # skip current LED
-
-        backtrack(0, 0, 0, 0)  # start recursion from LED 0
-        return res
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        ans = []
+        for hours in range(12):
+            for minutes in range(60):
+                if bin(hours).count('1') + bin(minutes).count('1') == turnedOn:
+                    time = f"{hours}:{minutes:02d}" 
+                    ans.append(time)
+        return ans
