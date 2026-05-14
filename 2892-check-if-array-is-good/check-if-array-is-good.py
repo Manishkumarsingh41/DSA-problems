6c55809e0d08d5179aedc01ca37e0d06669a6d62
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2026-05-14 21:08:57 +0530

    Time: 4 ms (11.03%) | Memory: 19.4 MB (17.79%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.py
+++ 2892-check-if-array-is-good/check-if-array-is-good.py
@@ -1,4 +1,4 @@
 class Solution:
     def isGood(self, nums: List[int]) -> bool:
-        return sorted(nums) == [*range(1,n:=len(nums)),n-1]
+        return {*nums}== {*range(1,n:=len(nums))} and nums.count(n-1) ==2
         
