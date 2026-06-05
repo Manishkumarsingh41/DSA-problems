54818083632cdc1fc327ed3c5dc0579231234eb8
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2026-06-05 16:02:00 +0530

    Time: 101 ms (59.64%) | Memory: 20.2 MB (51.81%) - LeetSync

---
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxAbsoluteSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_end = 0
+        min_end = 0
+
+        max_sum = 0
+        min_sum = 0
+
+        for num in nums:
+            max_end = max(num, max_end + num)
+            min_end = min(num, min_end + num)
+
+            max_sum = max(max_sum, max_end)
+            min_sum = min(min_sum, min_end)
+
+        return max(max_sum, -min_sum)
